9ce599561e5d2d870563e80c042c8fd16b225638
Jade Michael Thornton <jademichael@jmthornton.net>
2026-04-02 16:19:46 -0500

    fix weather condition grammar in readme
    map API condition strings (e.g. "Moderate Rain") to
    grammatically correct predicates (e.g. "moderately
    raining") so the readme sentence reads naturally.

--- .github/update-weather.py
+++ .github/update-weather.py
@@ -26,6 +26,28 @@ icons = {
   "windy": "🍃"
 }
 
+conditions = {
+    "Clear":            "clear",
+    "Partly Cloudy":    "partly cloudy",
+    "Mostly Cloudy":    "mostly cloudy",
+    "Cloudy":           "cloudy",
+    "Very Light Rain":  "very lightly raining",
+    "Light Rain":       "lightly raining",
+    "Moderate Rain":    "moderately raining",
+    "Heavy Rain":       "heavily raining",
+    "Very Heavy Rain":  "very heavily raining",
+    "Light Snow":       "lightly snowing",
+    "Moderate Snow":    "moderately snowing",
+    "Heavy Snow":       "heavily snowing",
+    "Light Sleet":      "lightly sleeting",
+    "Moderate Sleet":   "moderately sleeting",
+    "Heavy Sleet":      "heavily sleeting",
+    "Thunderstorm":     "thunderstorming",
+    "Fog":              "foggy",
+    "Windy":            "windy",
+    "Light Wind":       "a little windy",
+}
+
 url = f'https://swd.weatherflow.com/swd/rest/better_forecast?station_id=' + os.environ['TEMPEST_STATION'] + '&token=' + os.environ['TEMPEST_PUT'] + '&units_temp=f'
 with urllib.request.urlopen(url) as response:
   data = response.read()
@@ -38,7 +60,8 @@ start_pos = profile.find('\u200B') + len('\u200B') # zero-width spaces
 end_pos = profile.find('\u200B', start_pos)
 
 temp = str(round(weather['current_conditions']['air_temperature'])) + '°' + weather['units']['units_temp'].upper()
-condition = icons[weather['current_conditions']['icon']] + ' ' + weather['current_conditions']['conditions']
+raw_condition = weather['current_conditions']['conditions']
+condition = icons[weather['current_conditions']['icon']] + ' ' + conditions.get(raw_condition, raw_condition.lower())
 profile = profile[:start_pos] + temp + ' and ' + condition + profile[end_pos:]
 
 overwrite = open('README.md', 'w', encoding='utf-8')
